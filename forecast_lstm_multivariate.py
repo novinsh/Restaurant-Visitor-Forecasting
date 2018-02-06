0057328039f806d578e279e6ee9b2e7499f66a46
Novin Shahroudi <novinsha@gmail.com>
2018-02-06 11:25:16 +0200

    Add possibility of resuming the training.

--- forecast_lstm_multivariate.py
+++ forecast_lstm_multivariate.py
@@ -2,12 +2,15 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder, MinMaxScaler
 from datetime import datetime
 from sklearn.metrics import mean_squared_error
 from scipy.ndimage.interpolation import shift
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import LSTM, Dense, Conv2D, MaxPooling2D
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
 from math import sqrt, floor
 from matplotlib import pyplot as plt
 from argparse import ArgumentParser 
 from dataset import *
+import glob
 
 
 if __name__ == "__main__":
@@ -17,24 +20,43 @@ if __name__ == "__main__":
     parser.add_argument('-g', '--train-on-validation', \
             help='train on validation set as well as training set', action="store_true")
     parser.add_argument('-r', '--split-rate', \
-            help='training set spliting rate', type=float, default=0.8)
+            help='training set split rate, reset would be validation', type=float, default=0.8)
     parser.add_argument('-e', '--epochs', type=int, default=10)
     parser.add_argument('-b', '--batch-size', type=int, default=1000)
-    parser.add_argument('-u', '--unit-size', type=int, default=100)
-    parser.add_argument('-l', '--hidden-layers', type=int, default=0)
-    parser.add_argument('-filter', '--filter', help='whether to filter or not', action="store_true")
-    parser.add_argument('-s', '--store_to_filter', help='filter the store and plot prediction result', \
+    parser.add_argument('-u', '--lstm-units', type=int, default=100)
+    parser.add_argument('-l', '--lstm-layers', type=int, default=1)
+    parser.add_argument('-s', '--store_to_filter', \
+            help='filter the store and plot prediction result', \
             nargs=1, default='air_754ae581ad80cc9f')
-    parser.add_argument('--predict1', help='make predition on validation set', action="store_true")
-    parser.add_argument('--predict2', help='make prediction on test set', action="store_true")
+    parser.add_argument('--predict-test-set', \
+            help='make prediction on test set and store result', action="store_true")
+    parser.add_argument('--predict-train-set', \
+            help='make prediction on train set', action="store_true")
+    parser.add_argument('-n', '--name', \
+            help='a name for the job, if model existing will continue fitting', required=True)
     args = parser.parse_args()
+
+    # assert the input aarguments
+    assert args.split_rate > 0.0 and args.split_rate <= 1.0 
+    assert args.epochs > 0
+    assert args.lstm_units > 1
+    assert args.lstm_layers > 0 
+
+    # execution mode report
+    if args.verbose:
+        print("--- Execution mode report ----")
+        print("Fit model on All training set") if args.split_rate < 1.0 else print("%.2f training set size" % args.split_rate)
+        print("running prediction for training set") if args.predict_train_set else None
+        print("running prediction for validation set") if args.split_rate < 0.1 else None
+        print("running prediction for test set") if args.predict_train_set else None
+        print("plots for filtered store: ", args.store_to_filter) 
 
     # load ready to work with data
     train, test = load_data()
 
     # -------------- Choose a store for visualization purpose ----------------#
     # print(train['air_store_id'].tail(3100))
-    store_to_filter = args.store_to_filter #"air_90f0efbb702d77b7"#"air_e9ebf7fc520ac76a"
+    store_to_filter = args.store_to_filter #"air_90f0efbb702d77b7" #"air_e9ebf7fc520ac76a"
     this_store = train['air_store_id'] == store_to_filter
     col_to_plot = [
             'visitors', 
@@ -91,6 +113,10 @@ if __name__ == "__main__":
     x_train = x_train[:train_sample_size,:]
     y_train = y_train[:train_sample_size]
 
+    if args.split_rate == 1.0:
+        assert x_valid.shape == (0, 50)
+        assert y_valid.shape == (0,)
+
     # reshape for the network's input
     x_train = x_train.reshape((x_train.shape[0], 1, x_train.shape[1]))
     x_valid = x_valid.reshape((x_valid.shape[0], 1, x_valid.shape[1]))
@@ -104,50 +130,90 @@ if __name__ == "__main__":
         print("y_valid: ", y_valid.shape)
         print("X_test: ", x_test.shape)
 
-    # design network
-    model = Sequential()
-    # try1:
-    # model.add(Conv2D(input_shape=(x_train.shape[1], x_train.shape[2]), filters=32, kernel_size=3, padding='same', activation='tanh'))
-    # model.add(MaxPooling2D(pool_size=(2,2)))
-    # model.add(LSTM(100))
-    # try2:
-    # model.add(LSTM(256, input_shape=(x_train.shape[1], x_train.shape[2]), return_sequences=True ))
-    # model.add(LSTM(256, input_shape=(x_train.shape[1], x_train.shape[2]), return_sequences=True ))
-    # model.add(LSTM(100))
-    # try3:
-    for i in range(args.hidden_layers-1):
-        model.add(LSTM(args.unit_size, input_shape=(x_train.shape[1], x_train.shape[2]), return_sequences=True ))
-    model.add(LSTM(args.unit_size, input_shape=(x_train.shape[1], x_train.shape[2])))
-    model.add(Dense(1))
-    model.compile(loss='mse', optimizer='adam')
+    model_files = glob.glob("%s-*.hdf5" % args.name)
+    initial_epoch = 0
+    model_file = ""
+    model = None
+
+    if len(model_files) > 0: # existing model 
+        epoch_files = []
+        for mf in model_files:
+            s = mf.split("-")
+            epoch = int(s[1])
+            epoch_files.append((epoch, mf))
+        epoch_files.sort(key = lambda t: t[0], reverse=True)
+        initial_epoch, model_file = epoch_files[0]
+        model = load_model(model_file)
+    else: # new model
+        # design network
+        model = Sequential()
+        # try1:
+        # model.add(Conv2D(input_shape=(x_train.shape[1], x_train.shape[2]), filters=32, kernel_size=3, padding='same', activation='tanh'))
+        # model.add(MaxPooling2D(pool_size=(2,2)))
+        # model.add(LSTM(100))
+        # try2:
+        # model.add(LSTM(256, input_shape=(x_train.shape[1], x_train.shape[2]), return_sequences=True ))
+        # model.add(LSTM(256, input_shape=(x_train.shape[1], x_train.shape[2]), return_sequences=True ))
+        # model.add(LSTM(100))
+        # try3:
+        if args.lstm_layers > 1:
+            for i in range(args.lstm_layers-1):
+                model.add(LSTM(args.lstm_units, input_shape=(x_train.shape[1], x_train.shape[2]), return_sequences=True ))
+        model.add(LSTM(args.lstm_units, input_shape=(x_train.shape[1], x_train.shape[2])))
+        model.add(Dense(1))
+
+    # print network info
+    if args.verbose:
+        print("\n--- Network Model Info ---")
+        print("\tEpochs: %d" % args.epochs)
+        print("\tBatch size: %d" % args.batch_size)
+        print("\tLSTM units: %d" % args.lstm_units)
+        print("\tLSTM layers: %d" % args.lstm_layers)
+
+    adam = Adam(lr=0.001)
+    model.compile(loss='mse', optimizer=adam)
+
+    # create checkpoint for the model
+    filepath = "%s-{epoch:02d}-{val_loss:.2f}.hdf5" % args.name
+    checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, \
+            save_best_only=True, mode='auto')#, period=1)
+    callbacks = [checkpoint] # keras callbacks list 
+
+    print("initial epoch: ", initial_epoch)
 
     # fit network
-    if args.split_rate == 1.0: # train on all data
-        history = model.fit(x_train, y_train, epochs=args.epochs, batch_size=args.batch_size, \
-                verbose=2, shuffle=False)
-    else: # train with validation 
-        history = model.fit(x_train, y_train, epochs=args.epochs, batch_size=args.batch_size, \
-                validation_data=(x_valid, y_valid), verbose=2, shuffle=False)
+    if initial_epoch < args.epochs:
+        v = 2 if args.verbose else 1
+        if args.split_rate == 1.0: # train on all data
+            print("fit the model on the whole training set") if args.verbose else None
+            history = model.fit(x_train, y_train, epochs=args.epochs, batch_size=args.batch_size, \
+                    verbose=v, shuffle=False, callbacks=callbacks, initial_epoch=initial_epoch)
+        else: # train with validation 
+            print("fit the model with validation") if args.verbose else None
+            history = model.fit(x_train, y_train, epochs=args.epochs, \
+                    batch_size=args.batch_size, validation_data=(x_valid, y_valid),\
+                    verbose=v, shuffle=False, callbacks=callbacks, initial_epoch=initial_epoch)
 
-    print(type(history))
-    print(history)
-    if args.split_rate < 1.0 and args.train_on_validation:
-        history = model.fit(x_train, y_train, epochs=args.epochs, batch_size=args.batch_size, \
-                validation_data=(x_valid, y_valid), verbose=2, shuffle=False)
-
-    # plot history
-    plt.plot(history.history['loss'], label='train')
-    plt.plot(history.history['val_loss'], label='test')
-    plt.legend()
-    plt.show()
-     
-    # make a prediction for test data
-    # yhat = model.predict(x_test)
+        if args.split_rate < 1.0 and args.train_on_validation:
+            history = model.fit(x_train, y_train, epochs=args.epochs, batch_size=args.batch_size, \
+                    validation_data=(x_valid, y_valid), verbose=2, shuffle=False)
+        # plot history
+        plt.plot(history.history['loss'], label='train')
+        plt.plot(history.history['val_loss'], label='test')
+        plt.legend()
+        plt.show()
+    else:
+        print("model is already trained") if args.verbose else None
+    
     yhat = model.predict(x_valid)
     # yhat = model.predict(x_train)
     visitors = np.absolute(np.expm1(yhat))
-    # test['visitors'] = visitors 
-    # test[['id','visitors']].to_csv('2lstm100_submission_1.csv', index=False, float_format='%.3f')
+
+    # make a prediction for test data
+    if args.predict_test_set:
+        yhat = model.predict(x_test)
+        test['visitors'] = visitors 
+        test[['id','visitors']].to_csv('.csv', index=False, float_format='%.3f')
 
     # some debuggings with validation data
     # print("#"*20)
@@ -165,7 +231,7 @@ if __name__ == "__main__":
     # rmse = sqrt(mean_squared_error(yhat, y_train))
     print('Test RMSE: %.3f' % rmse)
 
-    # --------------- cotninue of the plot from top -------------------- #
+    # --------------- cotninue of the plot from above -------------------- #
     fig = plt.figure()
     fig.suptitle('input features for store: %s' % store_to_filter)
 
@@ -191,11 +257,12 @@ if __name__ == "__main__":
 
     plt.xlabel('Time step')
     plt.show()
-    # --------------- end of the plot from top -------------------- #
+    # --------------- end of the plot from above -------------------- #
 
-
-    # plot on validation adn prediction
-    plt.plot(np.absolute(np.expm1(y_valid)))
-    # plt.plot(np.absolute(np.expm1(y_train)))
-    plt.plot(visitors)
-    plt.show()
+    # plot on validation set 
+    if args.split_rate < 1.0: # if there was a validation splitage
+        # plot on validation and prediction
+        plt.plot(np.absolute(np.expm1(y_valid)))
+        # plt.plot(np.absolute(np.expm1(y_train)))
+        plt.plot(visitors)
+        plt.show()
